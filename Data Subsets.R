755632f89b60effdffa9b9cf7e7d876a4ccd6919
Rachel Coleman <coleman.rachel.e@gmail.com>
2021-07-14 22:12:10 -0500

    Updates

--- Data Subsets.R
+++ Data Subsets.R
@@ -1,20 +1,44 @@
-# Clean data, create data sets for project
+# Clean data, create data sets, load libraries
+
+# install.packages("haven")
+# install.packages("survey")
+# install.packages("jtools")
+# install.packages("remotes")
+# remotes::install_github("carlganz/svrepmisc")
+
+# library("haven")
+# library("survey")
+# library("jtools")
+# library("remotes")
+# library("svrepmisc")
 
 # PFI data: Replace all -1s with NAs, change grade level numbers
 # pfi_pu_pert[pfi_pu_pert == -1] <- NA
 # pfi_pu_pert$ALLGRADEX <- c(pfi_pu_pert$ALLGRADEX -3)
 # pfi_pu_pert$ALLGRADEX[pfi_pu_pert$ALLGRADEX == -1] <- 0
 
-# Create homeschool data subset
+# For general PFI data file, input details into survey package
+PFIdesign <- svrepdesign(data = pfi_pu_pert, repweights = subset(pfi_pu_pert, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+summary(PFIdesign)
+
+# Create homeschool data subset, input details into survey package
 HSData <- subset(pfi_pu_pert, HOMESCHLX == 1)
-# Remove students who "attend" school 25 hours or more
 HSData <- subset(HSData, HSData$SCHLHRSWK != 4 | is.na(HSData$SCHLHRSWK))
+HSdesign <- svrepdesign(data = HSData, repweights = subset(HSData, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+summary(HSdesign)
+# Note: 2nd line above removed students who "attend" school 25 hours or more
 
-# Create public school data subset
+# Create public school data subset, input details into survey package
 PublicSchool <- subset(pfi_pu_pert, EDCPUB == 1)
+PuSdesign <- svrepdesign(data = PublicSchool, repweights = subset(PublicSchool, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+summary(PuSdesign)
 
-# Create virtual school data subset
+# Create virtual school data subset, input details into survey package
 VirtualSchool <- subset(pfi_pu_pert, EDCINTK12 == 1)
+VSdesign <- svrepdesign(data = VirtualSchool, repweights = subset(VirtualSchool, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+summary(VSdesign)
 
-# Create private school data subset
+# Create private school data subset, input details into survey package
 PrivateSchool <- subset(pfi_pu_pert, EDCCAT == 1 | EDCREL == 1 | EDCPRI == 1)
+PrSdesign <- svrepdesign(data = PrivateSchool, repweights = subset(PrivateSchool, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+summary(PrSdesign)
--- Parent Education.R
+++ Parent Education.R
@@ -1,10 +1,104 @@
-svymean(~PARGRADEX == 1, PuSdesign, na.rm = TRUE)
+# Parent education, by school type
+# This script is dependent on the "Data Subsets" script
+
+# Run parental education levels, for each educational method
+
+# PFI overall, by parent education level grouping
+svymean(~PARGRADEX == 1, PFIdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 2, PFIdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, PFIdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 4, PFIdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 5, PFIdesign, na.rm = TRUE)
+
+# Homeschool overall, by parent education level grouping
 svymean(~PARGRADEX == 1, HSdesign, na.rm = TRUE)
-svymean(~PARGRADEX == 2, PuSdesign, na.rm = TRUE)
 svymean(~PARGRADEX == 2, HSdesign, na.rm = TRUE)
-svymean(~PARGRADEX == 3, PuSdesign, na.rm = TRUE)
 svymean(~PARGRADEX == 3, HSdesign, na.rm = TRUE)
-svymean(~PARGRADEX == 4, PuSdesign, na.rm = TRUE)
 svymean(~PARGRADEX == 4, HSdesign, na.rm = TRUE)
-svymean(~PARGRADEX == 5, PuSdesign, na.rm = TRUE)
 svymean(~PARGRADEX == 5, HSdesign, na.rm = TRUE)
+
+# Public school overall, by parent education level grouping
+svymean(~PARGRADEX == 1, PuSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 2, PuSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, PuSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 4, PuSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 5, PuSdesign, na.rm = TRUE)
+
+# Private school overall, by parent education level grouping
+svymean(~PARGRADEX == 1, PrSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 2, PrSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, PrSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 4, PrSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 5, PrSdesign, na.rm = TRUE)
+
+# Virtual school overall, by parent education level grouping
+svymean(~PARGRADEX == 1, VSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 2, VSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, VSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 4, VSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 5, VSdesign, na.rm = TRUE)
+
+
+# Testing looking at different education levels
+
+# Create education level data subset, input details into survey package
+
+# Elementary school
+homeES <- subset(HSData, ((ALLGRADEX == 1) | (ALLGRADEX == 2) | (ALLGRADEX == 3) | (ALLGRADEX == 4) | (ALLGRADEX == 5)))
+homeESdesign <- svrepdesign(data = homeES, repweights = subset(homeES, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+pubES <- subset(pfi_pu_pert, ((ALLGRADEX == 1) | (ALLGRADEX == 2) | (ALLGRADEX == 3) | (ALLGRADEX == 4) | (ALLGRADEX == 5)))
+pubESdesign <- svrepdesign(data = pubES, repweights = subset(pubES, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+priES <- subset(PrivateSchool, ((ALLGRADEX == 1) | (ALLGRADEX == 2) | (ALLGRADEX == 3) | (ALLGRADEX == 4) | (ALLGRADEX == 5)))
+priESdesign <- svrepdesign(data = priES, repweights = subset(priES, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+
+# Middle school
+homeMS <- subset(HSData, ((ALLGRADEX == 6) | (ALLGRADEX == 7) | (ALLGRADEX == 8)))
+homeMSdesign <- svrepdesign(data = homeMS, repweights = subset(homeMS, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+pubMS <- subset(pfi_pu_pert, ((ALLGRADEX == 6) | (ALLGRADEX == 7) | (ALLGRADEX == 8)))
+pubMSdesign <- svrepdesign(data = pubMS, repweights = subset(pubMS, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+priMS <- subset(PrivateSchool, ((ALLGRADEX == 6) | (ALLGRADEX == 7) | (ALLGRADEX == 8)))
+priMSdesign <- svrepdesign(data = priMS, repweights = subset(priMS, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+
+# High school
+  homeHS <- subset(HSData, ALLGRADEX > 8)
+homeHSdesign <- svrepdesign(data = homeHS, repweights = subset(homeHS, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+  pubHS <- subset(pfi_pu_pert, ALLGRADEX > 8)
+pubHSdesign <- svrepdesign(data = pubHS, repweights = subset(pubHS, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+  priHS <- subset(PrivateSchool, ALLGRADEX > 8)
+priHSdesign <- svrepdesign(data = priHS, repweights = subset(priHS, select = FPWT1:FPWT80), weights= ~FPWT, type="JK1", mse=TRUE, combined.weights=TRUE, scale=79/80)
+
+
+# Bachelor's degree, compared: 
+svymean(~PARGRADEX > 3, homeESdesign, na.rm = TRUE)
+svymean(~PARGRADEX > 3, pubESdesign, na.rm = TRUE)
+
+svymean(~PARGRADEX > 3, homeMSdesign, na.rm = TRUE)
+svymean(~PARGRADEX > 3, pubMSdesign, na.rm = TRUE)
+
+svymean(~PARGRADEX > 3, homeHSdesign, na.rm = TRUE)
+svymean(~PARGRADEX > 3, pubHSdesign, na.rm = TRUE)
+
+# Vocational or some college, compared: 
+svymean(~PARGRADEX == 3, homeESdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, pubESdesign, na.rm = TRUE)
+
+svymean(~PARGRADEX == 3, homeMSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, pubMSdesign, na.rm = TRUE)
+
+svymean(~PARGRADEX == 3, homeHSdesign, na.rm = TRUE)
+svymean(~PARGRADEX == 3, pubHSdesign, na.rm = TRUE)
+
+# High school diploma or less, compared:
+svymean(~PARGRADEX < 3, homeESdesign, na.rm = TRUE)
+svymean(~PARGRADEX < 3, pubESdesign, na.rm = TRUE)
+
+svymean(~PARGRADEX < 3, homeMSdesign, na.rm = TRUE)
+svymean(~PARGRADEX < 3, pubMSdesign, na.rm = TRUE)
+
+svymean(~PARGRADEX < 3, homeHSdesign, na.rm = TRUE)
+svymean(~PARGRADEX < 3, pubHSdesign, na.rm = TRUE)
+
+
+
+
+
